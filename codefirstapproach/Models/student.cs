19ef323fb721fc44c7422a537ca8ef6451f5e959
delll <delll@DESKTOP-FHD2Q16>
2024-11-01 21:21:41 +0545

    Add project files.

---
+++ codefirstapproach/Models/student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace codefirstapproach.Models
+{
+    public class student
+    {
+        [Key]
+        public int ID { get; set; }
+        [Column("StudentName",TypeName="varchar(200)")]
+        public string Name { get; set; }
+
+        [Column("StudentGender", TypeName = "varchar(200)")]
+
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ codefirstapproach/Models/studentdbcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace codefirstapproach.Models
+{
+    public class studentdbcontext:DbContext
+    {
+        public studentdbcontext (DbContextOptions options):base(options) 
+        {
+
+        }
+
+        public DbSet<student>students { get; set; }
+
+    }
+}
